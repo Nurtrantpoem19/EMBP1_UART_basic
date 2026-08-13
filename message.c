2f89fad3aa6d256a6b2d6d0a6500d8fedea08104
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-08-13 18:19:36 -0400

    fixed UART_init, set up baud rate and framing
    framing is not done yet, must still look further in the datasheet
    register page for the right bits

--- message.c
+++ message.c
@@ -1,16 +1,20 @@
+#define FOSC 16000000UL 
+#define BAUD 9600
+#define MYUBRR (FOSC / 16 / BAUD - 1)
+
+
+
 #include <avr/io.h>
 
-void UART_Init(void);
+void UART_Init(unsigned int ubrr);
+
+
 int main(void)
 {
-	UART_Init();
+	UART_Init(MYUBRR);
 
 }
-	void UART_Init(void)
-	{
-		UCSR0C | (1 << UMSEL01);
 
-		return;
 
 
 	}
