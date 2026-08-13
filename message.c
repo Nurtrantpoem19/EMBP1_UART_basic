d126a8b7960ec7e8e59e191b1405cace9129beaa
Nurtrantpoem19 <nurtrantpoem19@gmail.com>
2026-08-13 18:42:35 -0400

    adding frame formatting and baud rate and argument

--- message.c
+++ message.c
@@ -16,5 +16,40 @@ int main(void)
 }
 
 
+void UART_Init(unsigned int ubrr) 
+{
+  UBRR0H = (unsigned char)(ubrr >> 8);
+  UBRR0L = (unsigned char)ubrr;
+  
+  UCSR0C &= ~(1 << UMSEL00 | 1 << UMSEL01);
+  
+  UCSR0C &= ~(1 << UPM01 | 1 << UPM00);
+  UCSR0C |= (1 << UPM01);
+
+
+
+  
+
+  return;
+}
+
+// #define FOSC 1843200// Clock Speed
+// #define BAUD 9600
+// #define (MYUBRR FOSC/16/BAUD-1)
+// void main( void )
+// {...
+// USART_Init ( MYUBRR );
+// ...} // main
+// void USART_Init( unsigned int ubrr){
+// /* Set baud rate */
+// UBRRH = (unsigned char)(ubrr>>8);
+// UBRRL = (unsigned char)ubrr;
+// /* Enable receiver and transmitter */
+// UCSRB = (1<<RXEN)|(1<<TXEN);
+// /* Set frame format: 8data, 2stop bit */
+// UCSRC = (1<<USBS)|(3<<UCSZ0);
+// } // USART_Init
+//
+// #include <avr/io.h>
 
 	}
